204f896f2effa5f7fbec77374eb6f1af64f909bd
devfarhancoder <farhangori89@gmail.com>
2026-03-11 00:06:08 +0530

    Remove OTP endpoints from scholarship API

--- routes/scholarship.js
+++ routes/scholarship.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {
-  submitScholarshipResponse,
-  checkMobile,
-  sendOTP,
-  verifyOTP,
-} = require("../controllers/scholarshipController");
+const { submitScholarshipResponse, checkMobile } = require("../controllers/scholarshipController");
 
 router.post("/", submitScholarshipResponse);
 router.get("/check/:mobile", checkMobile);
-router.post("/send-otp", sendOTP);
-router.post("/verify-otp", verifyOTP);
 
 module.exports = router;
